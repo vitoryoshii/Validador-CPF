9881ec3f8db6d7ab981ea8e3a07d3fb407c3356e
Vitor Yoshii <100173103+vitoryoshii@users.noreply.github.com>
2025-04-07 21:45:14 -0300

    Add files via upload

---
+++ validadroCPF/validadorCPF.py
@@ -0,0 +1,48 @@
+# Função validarCPF 
+
+def validarCPF(cpf):
+    
+    #Retira caracteres (. e -)
+    cpf = cpf.replace(".", "").replace("-", "")
+    
+    #Valida se CPF tem 11 digitos
+    if len(cpf) != 11:
+        return False
+    
+    #Verifica primeiro digito
+    soma_num_cpf = 0 
+
+    for i in range(0,9):
+        soma_num_cpf += int(cpf[i]) * (10 - i)
+
+    resto_one = (soma_num_cpf * 10) % 11
+    if resto_one == 10:
+        resto_one = 0
+
+    if resto_one != int(cpf[9]):
+        return False
+    
+    #Verifica segundo digito
+    soma_num_cpf = 0
+
+    for i in range(0,10):
+        soma_num_cpf += int(cpf[i]) * (11 - i)
+    
+    resto_two = (soma_num_cpf * 10) % 11
+    if resto_two == 10:
+        resto_two = 0
+
+    if resto_two != int(cpf[10]):
+        return False    
+        
+    return True
+
+# Pede CPF do Usuário e valida a Função Validadar CPF
+print("VALIDADOR CPF")
+print()
+cpf = input("DIGITE O SEU CPF (000.000.000-00): ")
+
+if validarCPF(cpf) == True:
+    print(f"CPF: {cpf} É VÁLIDO!")
+else:
+    print(f"CPF: {cpf} NÃO É VÁLIDO!")
